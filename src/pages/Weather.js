1d95560be139cc736e41c9c011de602866802ad6
Ivan M <im@kalite.ro>
2023-04-28 18:02:04 +0300

    add weather citises

--- src/pages/Weather.js
+++ src/pages/Weather.js
@@ -2,9 +2,19 @@ import React from "react";
 import "../css/Weather.css";
 import { Footer, Header } from "../components/index";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAngleLeft, faSearch } from "@fortawesome/free-solid-svg-icons";
+import {
+  faAngleLeft,
+  faSearch,
+  faAngleRight,
+} from "@fortawesome/free-solid-svg-icons";
 
 function Weather() {
+  const [cities, setCities] = React.useState(
+    localStorage.getItem("cities") === null
+      ? []
+      : JSON.parse(localStorage.getItem("cities"))
+  );
+
   function getWeather() {
     const q = document.getElementById("city").value;
     fetch(
@@ -18,9 +28,27 @@ function Weather() {
         console.log("Ощущается, как - " + Math.ceil(resp.main.feels_like));
         document.getElementById("weather-result-temp").innerHTML = temp;
         document.getElementById("weather-result-feels-p").innerHTML = feels;
-        document.getElementsByClassName("weather-results")[0].style.display = "flex";
+        document.getElementsByClassName("weather-results")[0].style.display =
+          "flex";
       })
     );
+  }
+
+  function changeCity(v) {
+    document.getElementById("city").value = v;
+    getWeather();
+  }
+
+  function addCity() {
+    const q = document.getElementById("city").value;
+    var list =
+      localStorage.getItem("cities") === null
+        ? []
+        : JSON.parse(localStorage.getItem("cities"));
+    list[list.length] = q;
+    console.log(list);
+    localStorage.setItem("cities", JSON.stringify(list));
+    setCities(list);
   }
 
   return (
@@ -34,6 +62,21 @@ function Weather() {
           className="weather-search"
           onClick={() => getWeather()}
         />
+      </div>
+
+      <div className="my-cities">
+        <div className="add-city">
+          <button onClick={() => addCity()}>Добавить город</button>
+        </div>
+        <div className="cities-list">
+          <b>Избранные города:</b>
+          {cities.map((v, k) => (
+            <div className="city" key={k} onClick={() => changeCity(v)}>
+              <p>{v}</p>
+              <FontAwesomeIcon icon={faAngleRight} className="weather-right" />
+            </div>
+          ))}
+        </div>
       </div>
 
       <div className="weather-results">
